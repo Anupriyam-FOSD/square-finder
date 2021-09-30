4f497adff773ca8dbe32f655657db6e6ce2b144b
Anupriyam Shukla <60869367+Anupriyam-FOSD@users.noreply.github.com>
2021-09-30 22:11:11 +0530

    added print command.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-#import math
+import math
 
 enter_num = float(input("Enter number: "))
-final_ans = enter_num**2
-
-# you need to print the ans of square and remove the import math line
+print("Final answer is :",final_ans = enter_num**2)
